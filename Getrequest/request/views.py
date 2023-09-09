88f73c1dfc51e37af29b7d990801d68a85652dd2
njokuchidinma <kimberlypeters301@gmail.com>
2023-09-09 15:12:20 +0100

    Done hopefully o

--- Getrequest/request/views.py
+++ Getrequest/request/views.py
@@ -4,11 +4,14 @@ from datetime import datetime,timezone
 
 def api(request):
     slack_name = request.GET.get("slack_name")
-    date_time = datetime.now(timezone.utc)
-    current_day_digit = date_time.weekday()
+    date = datetime.now(timezone.utc)
+    date_time = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
+    current_day_digit = date.weekday()
     days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
     current_day = days[current_day_digit]
-    utc_time = date_time.replace(tzinfo=timezone.utc)
+    # utc_time = date_time.replace(tzinfo=timezone.utc)
+    utc_time = date_time
+
     track = request.GET.get("track")
    
     return JsonResponse(
@@ -17,7 +20,9 @@ def api(request):
          'utc_time': utc_time, 
          'track': track,
          'github_file_url': 'https://github.com/njokuchidinma/HNGxSTAGE1/tree/main/Getrequest',
-         'github_repo_url': 'https://github.com/njokuchidinma/HNGxSTAGE1.git',
+         'github_repo_url': 'https://github.com/njokuchidinma/HNGxSTAGE1',
          'status_code': 200,
         }
     )
+
+    datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
